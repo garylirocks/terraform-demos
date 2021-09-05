57074e89ac6f0bc19534761b77fc0bd4584b3d34
garylirocks <gary.li.rocks@gmail.com>
2021-09-05 22:05:51 +1200

    Add a backend block

---
+++ README.md
@@ -0,0 +1,2 @@
+
+- Code in `remote-storage` create an Azure blob container to store remote state file for `main.tf`
--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "2.75.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "learning-rg"
+    storage_account_name = "tfstatey2hkc"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
