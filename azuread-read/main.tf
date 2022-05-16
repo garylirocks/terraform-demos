1dc141f7f02bb546678397168f7a2201362e240e
garylirocks <gary.li.rocks@gmail.com>
2022-05-16 18:19:34 +1200

    Update azuread demo

--- azuread-read/main.tf
+++ azuread-read/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "> 2.75.0"
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.22.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "azuread" {
+  # Configuration options
 }
 
 variable "user_principal_name" {
@@ -22,4 +22,13 @@ data "azuread_user" "example" {
 
 output "given_name" {
   value = data.azuread_user.example.given_name
+}
+
+# create an application
+resource "azuread_application" "example" {
+  display_name = "example"
+}
+
+output "application_name" {
+  value = azuread_application.example.application_id
 }
