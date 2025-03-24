e4608da3859197d8c9b185af472d94b7c9aa30bb
garylirocks <gary.li.rocks@gmail.com>
2025-03-25 11:22:08 +1300

    Update Linux VM demo

--- vm-linux/locals.tf
+++ vm-linux/locals.tf
@@ -1,8 +1,8 @@
 locals {
-  name     = "demo-001"
+  name     = "linux-vm-demo-001"
   location = "australiaeast"
   vnet = {
-    address_space           = ["10.0.0.0/16"]
-    subnet_address_prefixes = ["10.0.0.0/24"]
+    address_space           = ["10.2.0.0/16"]
+    subnet_address_prefixes = ["10.2.0.0/24"]
   }
 }
